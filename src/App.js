5173616a505cb66bfd91059dcbac1f958884a20f
Hesoyam91 <gerv.herrera@duocuc.cl>
2023-05-20 12:41:27 -0400

    Tarjetas añadidas

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PaymentForm from './PaymentForm';
+import PaymentForms from './PaymentForms';
+
+function App() {
+  return (
+      <div className="layout">
+          <PaymentForms />
+      </div>
+  );
+}
+
+export default App;
